ed44d85c066e9cbfce001d9c3c658787cd8b05b6
Brett VanderVeen <brett.m.veen@gmail.com>
2017-07-05 23:58:16 -0400

    add redux to the mix

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,25 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { createStore, Store as ReduxStore } from 'redux'
 
-import { Hello } from "./components/Hello";
+import { reducers, Store } from './reducers'
+import { incrementCounter, resetCounter } from './actions'
+import { Hello } from './components/Hello'
+
+const store: ReduxStore<Store.All> = createStore(reducers)
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(incrementCounter(1)) // { counter: { value: 1 } }
+store.dispatch(incrementCounter(1)) // { counter: { value: 2 } }
+store.dispatch(incrementCounter(1)) // { counter: { value: 3 } }
+store.dispatch(resetCounter())      // { counter: { value: 0 } }
+store.dispatch(incrementCounter(1)) // { counter: { value: 1 } }
+
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("root")
-);
+    <Hello compiler='TypeScript' framework='React' />,
+    document.getElementById('root'),
+)
---
+++ src/reducers/index.ts
@@ -0,0 +1,33 @@
+import { combineReducers } from 'redux'
+
+import { Action } from '../actions'
+
+export namespace Store {
+
+  export type Counter = { value: number }
+
+  export type All = {
+    counter: Counter,
+  }
+}
+
+const initialState: Store.Counter = {
+  value: 0,
+}
+
+function counter (state: Store.Counter = initialState, action: Action): Store.Counter {
+  const { value } = state
+  switch (action.type) {
+    case 'INCREMENT_COUNTER':
+      const newValue = value + action.delta
+      return { value: newValue }
+    case 'RESET_COUNTER':
+      return { value: 0 }
+  }
+
+  return state
+}
+
+export const reducers = combineReducers<Store.All>({
+  counter,
+})
